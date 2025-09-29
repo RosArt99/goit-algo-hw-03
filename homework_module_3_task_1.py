ef53066c9d66f14f8d609cc07000e00e0fadeda6
Ros <kukishvmasle@gmail.com>
2025-09-29 19:24:32 +0200

    Removed the looper and input request.

--- homework_module_3_task_1.py
+++ homework_module_3_task_1.py
@@ -1,17 +1,10 @@
 from datetime import datetime
 
-def get_days_from_today(): #date was removed from function arguments. Incorporated in function instead.
-    while True: #turning on loop
-        try: 
-            date = str(input("Enter the date in format \"YYYY-MM-DD\": ")) #\ to ignore "
-            converted_time = datetime.strptime(date, "%Y-%m-%d").date() #place where ValueError occurs 
-        except ValueError:
-            print("Invalid time format, try again, please, use \"YYYY-MM-DD\" format.")
-        else:
-            today = datetime.today().date()
-            days_ahead = (converted_time - today).days #days_ahead is timedelta object, .days is an attribute
-            return days_ahead
+def get_days_from_today(date: str) -> int: #date is back to the function
+        converted_time = datetime.strptime(date, "%Y-%m-%d").date() #converted_time is string in datetime object 
+        today = datetime.today().date() #adding .date() as attribute to get object without hours
+        days_ahead = (converted_time - today).days #days_ahead is timedelta object, .days is an attribute
+        return days_ahead #function returns calculated days difference. No loopers, no input request.
 
 
-print(get_days_from_today())
-
+print(get_days_from_today("2025-09-30"))
