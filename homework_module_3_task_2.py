b8a8684d94a355252f469061061057e6bb9d0f8e
Ros <kukishvmasle@gmail.com>
2025-09-29 20:02:41 +0200

    Removed the looper and input request. Function returns empty list if conditions are not met.

--- homework_module_3_task_2.py
+++ homework_module_3_task_2.py
@@ -1,31 +1,16 @@
 import random
 
-def get_numbers_ticket():
-    while True: #turning on looper
-        try:
-            min = int(input("Enter minimum number: "))
-            if min < 1:
-                print(f"Minimum number can\'t be negative or equal to zero, you entered:{min}")
-                continue
-            if min > 999:
-                print(f"Minimum number can\'t be higher than 999, you entered:{min}")
-                continue
-            max = int(input("Enter maximum number: "))
-            if max > 1000:
-                print(f"Maximum number can\'t be higher than 1000, you entered:{max}")
-                continue
-            if max < 1:
-                print(f"Maximum number can\'t negative or equal to zero, you entered:{max}")
-                continue
-            quantity = int(input("Enter quantity between min and max: "))         
-            if quantity > (max - min+1):
-                print(f"Quantity is too large, reduce quantity for provided range min:{min}, max{max}")
-                continue
-            random_list = random.sample(range(min, max+1), quantity)
-            random_list.sort()
-            return random_list
-        except ValueError:
-            print("Enter integers only!")
+def get_numbers_ticket(min, max, quantity): #arguments are back to the function
+    if min < 1 or max > 1000: 
+        print("min can't be negative, max can't be higher than 1000")
+        return []
+    if quantity > ((max+1) - min):
+        print("incorrect range")
+        return []
+    else: 
+        random_list = random.sample(range(min, max+1), quantity)
+        random_list.sort()
+    return random_list
 
-print(get_numbers_ticket())
+print(get_numbers_ticket(1, 59, 10))
 
