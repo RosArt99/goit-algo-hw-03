bf938b8ccce535a3fb3d6b2f00c4ceb03799f0f3
Ros <kukishvmasle@gmail.com>
2025-10-02 10:53:00 +0200

    Try and Except were added to handle the ValueError

--- homework_module_3_task_1.py
+++ homework_module_3_task_1.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
 def get_days_from_today(date: str) -> int: #date is back to the function
-        converted_time = datetime.strptime(date, "%Y-%m-%d").date() #converted_time is string in datetime object 
+        try:
+                converted_time = datetime.strptime(date, "%Y-%m-%d").date() #converted_time is string in datetime object
+        except ValueError:        
+                return "Invalid time format, try again, please, use \"YYYY-MM-DD\" format."
+         
         today = datetime.today().date() #adding .date() as attribute to get object without hours
         days_ahead = (converted_time - today).days #days_ahead is timedelta object, .days is an attribute
         return days_ahead #function returns calculated days difference. No loopers, no input request.
-
-
-print(get_days_from_today("2025-09-30"))
+        
+print(get_days_from_today("2026-03-26"))
